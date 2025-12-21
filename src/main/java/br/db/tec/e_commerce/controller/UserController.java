d681892179b797c1ff36627a5af39771fb65ff66
renansalves <renan.alves@edu.pucrs.br>
2025-12-21 18:52:16 -0300

    Feat
    Adicionando anotações para documentação com swagger.

--- src/main/java/br/db/tec/e_commerce/controller/UserController.java
+++ src/main/java/br/db/tec/e_commerce/controller/UserController.java
@@ -2,6 +2,8 @@ package br.db.tec.e_commerce.controller;
 
 import br.db.tec.e_commerce.dto.user.UserRegisterRequestDTO;
 import br.db.tec.e_commerce.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,11 +12,16 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "Users", description = "Endpoint responsavel por gerenciar authenticação dos usuarios.")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(
+    summary = "Registra novo usuário.",
+    description = "Realiza o registro do usuario com as informações de nome e email."
+    )
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody @Valid UserRegisterRequestDTO dto) {
         userService.register(dto);
